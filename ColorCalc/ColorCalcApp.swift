0c97e3c9499a7bc65b0d159360e59377af0eb55f
Ian <nonzeronucleus@ianplumb.org>
2022-06-13 20:06:18 +0100

    Initial Commit

---
+++ ColorCalc/ColorCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCalcApp.swift
+//  ColorCalc
+//
+//  Created by Ian Plumb on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
