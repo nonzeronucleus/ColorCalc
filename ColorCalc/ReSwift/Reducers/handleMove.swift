dbbf65416d763b7dfe0f4a0061dae35588f8882e
Ian <nonzeronucleus@ianplumb.org>
2022-06-14 19:07:31 +0100

    Temp

--- ColorCalc/ReSwift/Reducers/handleMove.swift
+++ ColorCalc/ReSwift/Reducers/handleMove.swift
@@ -1,7 +1,7 @@
 import ReSwift
 
 func handleMove(state:GameState, action:MoveAction) -> GameState {
-    var state = state
+    let state = state
     
 //    let paint = state.tubes[action.from].
 
